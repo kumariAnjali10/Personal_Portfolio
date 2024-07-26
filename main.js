fd92df0615bcc36f0575476fc74c23f8dc337d31
Kumari Anjali <143901044+kumariAnjali10@users.noreply.github.com>
2024-07-26 22:24:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['FrontEnd Developer', ' MERN Developer', ' Competitive Programmer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
